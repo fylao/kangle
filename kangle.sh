c545eb74fb47478b9434c48010af7676162887cd
飞鱼佬 <fy@love.edu.pl>
2019-07-11 19:55:38 +0800

    Delete kangle.sh

--- kangle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export START="kangle.fylao.com"
-export UPTIME="$(curl -s http://kangle.fylao.com/uptime.txt)"
-rm -rf kangle-start.sh kangle-start.* kangle.sh kangle.*
-wget ${START}/kangle-start.sh -O kangle-start.sh
-sh kangle-start.sh
